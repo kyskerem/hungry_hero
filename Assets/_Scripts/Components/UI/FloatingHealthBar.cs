de8003e8787fc73ccfde7a2d55593123fa5f443e
Kerem <keremkys16@gmail.com>
2024-02-04 17:36:15 +0300

    close health bar at the beginning

--- Assets/_Scripts/Components/UI/FloatingHealthBar.cs
+++ Assets/_Scripts/Components/UI/FloatingHealthBar.cs
@@ -7,12 +7,22 @@ public class FloatingHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private HealthComponent healthComponent;
+    [SerializeField] private bool showAtBeginning = false;
+    private bool isActive;
     void Awake()
     {
         healthComponent.OnHealthChanged += ChangeHealth;
+        gameObject.SetActive(showAtBeginning);
+        isActive = showAtBeginning;
     }
     void ChangeHealth(float currentHealth, float maxHealth)
     {
+        // don't show if enemy is not damaged
+        if (!isActive && currentHealth != maxHealth)
+        {
+            isActive = true;
+            gameObject.SetActive(true);
+        }
         slider.value = currentHealth / maxHealth;
     }
 }
